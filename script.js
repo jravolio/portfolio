38bcabc1803005c4f42bce2dc6c34b4bcbbcbfff
jravolio <jravolio892@gmail.com>
2022-03-22 17:21:07 -0300

    finished mobile design

--- script.js
+++ script.js
@@ -44,17 +44,37 @@ class MobileNavbar {
   mobileNavbar.init();
 
 // close menu
-const links = document.querySelectorAll('nav ul li a')
-const navDiv = document.querySelector('.navbar div')
-const ul = document.querySelector('.navbar ul.nav-list')
-const liAnimation = document.querySelectorAll('.navbar ul.nav-list li')
-
-for (const link of links){
-  link.addEventListener('click', function(){
-    navDiv.classList.remove('active')
-    ul.classList.remove('active')
-    for(const li of liAnimation){
-      li.removeAttribute("style")
-    }
-    })
+function takeOffAttributesOfMenu(){
+  const links = document.querySelectorAll('nav ul li a')
+  const navDiv = document.querySelector('.navbar div')
+  const ul = document.querySelector('.navbar ul.nav-list')
+  const liAnimation = document.querySelectorAll('.navbar ul.nav-list li')
+  
+  for (const link of links){
+    link.addEventListener('click', function(){
+      navDiv.classList.remove('active')
+      ul.classList.remove('active')
+      for(const li of liAnimation){
+        li.removeAttribute("style")
+      }
+      })
+  }
 }
+
+takeOffAttributesOfMenu()
+
+//SWIPER
+const swiper = new Swiper('.swiper-container',{
+  slidesPerView: 1,
+  pagination: {
+      el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+      767:{
+          slidesPerView: 2,
+          setWrapperSize: true
+      }
+  }
+})
