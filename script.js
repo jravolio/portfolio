530823851eebb7d188457ee7d39c91da653673a8
jravolio <jravolio892@gmail.com>
2022-03-23 15:10:48 -0300

    double swiper working

--- script.js
+++ script.js
@@ -63,23 +63,9 @@ function takeOffAttributesOfMenu(){
 
 takeOffAttributesOfMenu()
 
-//SWIPER COVERFLOW
-const swiper1 = new Swiper('.swiper-container1',{
-  slidesPerView: 1,
-  pagination: {
-      el: '.swiper-pagination1'
-  },
-  mousewheel: true,
-  keyboard: true,
-  breakpoints: {
-      767:{
-          slidesPerView: 2,
-          setWrapperSize: true
-      }
-  }
-})
 
 //SWIPER
+
 const swiper = new Swiper('.swiper-container',{
   slidesPerView: 1,
   pagination: {
